a419948b314bd3885a6241eeb0d893b0599e8d65
FlorinaCiuchete <165383420+FlorinaCiuchete@users.noreply.github.com>
2024-05-09 11:37:37 +0300

    update snapshot, comp fisiere sterse sau modif
    Snapshot e fisierul vechi, daca nu exista se creeaza si se iese din functie, altfel se creeaza un fisier temporar temp dupa care se compara cu vechiul fisier snapshot. In final snapshot devine temp si diferentele sunt afisate

--- proiect_so.c
+++ proiect_so.c
@@ -52,86 +52,131 @@ void process_directory(const char *dir_name, int snapshot_fd)
   closedir(dirp);
 }
 
-void create_snapshot(const char *dir_name)
-{
-  char snapshot_file[PATH_MAX];
-  snprintf(snapshot_file, PATH_MAX, "%s/%s.txt", dir_name, basename((char *)dir_name));
-  
-  int snapshot_fd;
-  if ((snapshot_fd = open(snapshot_file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
-    {
-      printf("Error: Cannot open output file\n");
-      return;
-    }
-  
-  process_directory(dir_name, snapshot_fd);
-  
-  close(snapshot_fd);
-  printf("Snapshot for directory %s created.\n", dir_name);
-}
 
 void compare_and_update_snapshot(const char *dir_name)
 {
   char snapshot_file[PATH_MAX];
-  snprintf(snapshot_file, PATH_MAX, "%s/%s.txt", dir_name, basename((char *)dir_name));
+  char temp_file[PATH_MAX];
+  char diff_output_file[PATH_MAX];
   
   int snapshot_fd;
+  snprintf(snapshot_file, PATH_MAX, "%s/%s_snapshot.txt", dir_name, basename((char *)dir_name));
   if ((snapshot_fd = open(snapshot_file, O_RDONLY)) == -1)
     {
       if (errno == ENOENT)
         {
-	  create_snapshot(dir_name);
+	  //create_snapshot(dir_name);
+	  if ((snapshot_fd = open(snapshot_file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+	    {
+	      printf("Error: Cannot open output file\n");
+	      return;
+	    }
+	  
+	  process_directory(dir_name, snapshot_fd);
+	  
+	  close(snapshot_fd);
+	  printf("Snapshot for directory %s created.\n", dir_name);
 	  return;
         }
       else
         {
-            printf("Error: Cannot open snapshot file %s\n", snapshot_file);
-            return;
+	  printf("Error: Cannot open snapshot file %s\n", snapshot_file);
+	  return;
         }
     }
-  
+  //daca nu a fost primul apel de creeare de snapshot, facem un fisier temporar cu care sa comparam apoi diferentele
   int temp_fd;
-    char temp_file[PATH_MAX];
-    snprintf(temp_file, PATH_MAX, "%s/%s.tmp", dir_name, basename((char *)dir_name));
-    
-    if ((temp_fd = open(temp_file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+  snprintf(temp_file, PATH_MAX, "%s/%s_temp.txt", dir_name, basename((char *)dir_name));
+  if ((temp_fd = open(temp_file, O_RDONLY)) == -1)
+    {
+      if (errno == ENOENT)
+        {
+	  if ((temp_fd = open(temp_file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+	    {
+	      printf("Error: Cannot open temp file\n");
+	      return;
+	    }
+	  
+	  process_directory(dir_name, temp_fd);
+	  
+	  close(temp_fd);
+	  printf("Temp file for directory %s created.\n", dir_name);
+        }
+      else
+        {
+	  printf("Error: Cannot open temp file %s\n", snapshot_file);
+	  return;
+        }
+    }
+
+  
+  snprintf(diff_output_file, PATH_MAX, "%s/%s_diff.txt", dir_name, basename((char *)dir_name));
+  
+  char command[2 * PATH_MAX + 50]; 
+  snprintf(command, sizeof(command), "diff -uN --strip-trailing-cr %s %s > /dev/null", snapshot_file, temp_file);
+  
+  if (system(command) == 0) {
+    printf("No changes found in %s\n", dir_name);
+    remove(temp_file);
+    remove(diff_output_file);
+  }
+  else{
+    printf("Changes found in %s. Updating snapshot.\n", dir_name);
+  }
+  
+  FILE *diff_fp = fopen(diff_output_file, "r");
+  if (diff_fp){
+    printf("Differences:\n");
+    char diff_line[MAX_BUFFER_SIZE];
+    while (fgets(diff_line, sizeof(diff_line), diff_fp)){
+      printf("%s", diff_line);
+    }
+    fclose(diff_fp);
+  }
+  
+  FILE *snapshot_fp = fopen(snapshot_file, "r");
+  FILE *temp_fp = fopen(temp_file, "r");
+  
+  if (snapshot_fp && temp_fp)
+    {
+      printf("Renamed or deleted files:\n");
+      char snapshot_line[MAX_BUFFER_SIZE];
+      char temp_line[MAX_BUFFER_SIZE];
+      
+      while (fgets(snapshot_line, sizeof(snapshot_line), snapshot_fp) && fgets(temp_line, sizeof(temp_line), temp_fp))
+	{
+	  if (strcmp(snapshot_line, temp_line) != 0)
+	    {
+		if (strstr(temp_line, snapshot_line) == NULL)
+		  {
+		    printf("Deleted: %s", snapshot_line);
+		  }
+		else
+		  {
+                    printf("Renamed: %s -> %s", snapshot_line, temp_line);
+		  }
+	    }
+	}
+      while (fgets(snapshot_line, sizeof(snapshot_line), snapshot_fp))
+	{
+	  printf("Deleted: %s", snapshot_line);
+	}
+      while (fgets(temp_line, sizeof(temp_line), temp_fp))
+	{
+	  printf("Renamed: %s", temp_line);
+	  }
+      
+      fclose(snapshot_fp);
+      fclose(temp_fp);
+      }
+  else
       {
-        printf("Error: Cannot create temporary file\n");
-        close(snapshot_fd);
-        return;
+	printf("Error: Cannot open snapshot files for comparison\n");
       }
-    
-    process_directory(dir_name, temp_fd);
-    
-    close(snapshot_fd);
-    close(temp_fd);
-    
-    char diff_output_file[PATH_MAX];
-    snprintf(diff_output_file, PATH_MAX, "%s/%s.diff", "snapshot_", dir_name);
-    
-    char command[2 * PATH_MAX + 50]; 
-    snprintf(command, sizeof(command), "diff -uN --strip-trailing-cr %s %s > /dev/null", snapshot_file, temp_file);
-    
-    if (system(command) == 0) {
-      printf("No changes found in %s\n", dir_name);
-      remove(temp_file);
-      remove(diff_output_file);
-    }
-    else{
-      printf("Changes found in %s. Updating snapshot.\n", dir_name);
-      rename(temp_file, snapshot_file);
-    }
-    
-    FILE *diff_fp = fopen(diff_output_file, "r");
-    if (diff_fp){
-      printf("Differences:\n");
-      char diff_line[MAX_BUFFER_SIZE];
-      while (fgets(diff_line, sizeof(diff_line), diff_fp)){
-	printf("%s", diff_line);
-      }
-      fclose(diff_fp);
-    }
+  
+  rename(temp_file, snapshot_file);
 }
+
 
 int main(int argc, char **argv){
   if (argc != 2){
@@ -144,5 +189,4 @@ int main(int argc, char **argv){
   compare_and_update_snapshot(dir_name);
 
   return 0;
-}
-
+} 
